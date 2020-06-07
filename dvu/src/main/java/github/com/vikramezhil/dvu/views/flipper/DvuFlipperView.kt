f3643c54f6daf826ddca2257c42bbd245c6ee432
vikramezhil <vikram.ezhil.1988@gmail.com>
2020-06-08 00:36:28 +0530

    added infinte scroll property, change swipe functionality

--- dvu/src/main/java/github/com/vikramezhil/dvu/views/flipper/DvuFlipperView.kt
+++ dvu/src/main/java/github/com/vikramezhil/dvu/views/flipper/DvuFlipperView.kt
@@ -1,12 +1,13 @@
 package github.com.vikramezhil.dvu.views.flipper
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.view.GestureDetector
 import android.view.MotionEvent
-import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.ViewFlipper
 import androidx.annotation.AttrRes
+import github.com.vikramezhil.dvu.R
 
 /**
  * Droid View Utils - Flipper View
@@ -16,29 +17,84 @@ import androidx.annotation.AttrRes
 class DvuFlipperView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, @AttrRes defStyleAttr: Int = 0): ViewFlipper(context)  {
 
     private val properties: DvuFvProps = object: DvuFvProps() {
-        override var swipeGestureDetector: GestureDetector?  = null
+        override var initialX: Float = 0.0f
+
+        override var infiniteSwipe: Boolean = false
+            set(value) {
+                field = value
+                invalidate()
+            }
     }
 
     private var listener: OnDvuFvListener? = null
 
     init {
-        // Initializing the swipe gesture detector
-        @Suppress("DEPRECATION")
-        properties.swipeGestureDetector = GestureDetector(DvuFvSGDetector(context, this, object: OnDvuFvListener {
-            override fun onCurrentViewVisible(view: View, position: Int) {
-                listener?.onCurrentViewVisible(view, position)
-            }
-        }))
+        init(context, attrs)
+    }
 
-        // Setting the touch listener and assigning the event to the swipe gesture detector
-        setOnTouchListener { _, event ->
-            properties.swipeGestureDetector?.onTouchEvent(event)
-            true
+    /**
+     * Initializes the view attributes
+     * @param context Context The view context
+     * @param attrs AttributeSet The view attributes
+     */
+    private fun init(context: Context, attrs: AttributeSet?) {
+        if (attrs == null) return
+
+        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.DvuFlipperView, 0, 0)
+
+        try {
+            properties.infiniteSwipe = typedArray.getBoolean(R.styleable.DvuFlipperView_dvuFvInfiniteSwipe, properties.infiniteSwipe)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            typedArray.recycle()
         }
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
         super.dispatchTouchEvent(ev)
+        return true
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let {
+            when(it.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    properties.initialX = it.x
+                }
+                MotionEvent.ACTION_UP -> {
+                    val finalX = it.x
+                    if (properties.initialX > finalX) {
+                        if ((displayedChild == (childCount - 1)) && !properties.infiniteSwipe) {
+                            // Reached end
+                            return false
+                        }
+
+                        inAnimation = AnimationUtils.loadAnimation(context, R.anim.left_in)
+                        outAnimation = AnimationUtils.loadAnimation(context, R.anim.left_out)
+                        showNext()
+                    } else {
+                        if (displayedChild == 0 && !properties.infiniteSwipe) {
+                            // Reached start
+                            return false
+                        }
+
+                        inAnimation = AnimationUtils.loadAnimation(context, R.anim.right_in)
+                        outAnimation = AnimationUtils.loadAnimation(context, R.anim.right_out)
+                        showPrevious()
+                    }
+
+                    // Sending an update back on the current visible view
+                    listener?.onCurrentViewVisible(currentView, displayedChild)
+                } else -> {
+                    return false
+                }
+            }
+        } ?: run {
+            return false
+        }
+
         return true
     }
 
--- dvu/src/main/java/github/com/vikramezhil/dvu/views/flipper/DvuFvProps.kt
+++ dvu/src/main/java/github/com/vikramezhil/dvu/views/flipper/DvuFvProps.kt
@@ -1,18 +1,12 @@
 package github.com.vikramezhil.dvu.views.flipper
 
-import android.view.GestureDetector
-
 /**
  * Droid View Utils - Flipper View Props
  * @author vikramezhil
  */
 
 abstract class DvuFvProps {
+    abstract var initialX: Float
 
-    companion object {
-        const val SWIPE_MIN_DISTANCE = 120
-        const val SWIPE_THRESHOLD_VELOCITY = 200
-    }
-
-    abstract var swipeGestureDetector: GestureDetector?
+    abstract var infiniteSwipe: Boolean
 }
