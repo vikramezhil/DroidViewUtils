10bd5bf6e55f3b05fdb5731f6321fe87e600f767
vikramezhil <vikram.ezhil.1988@gmail.com>
2020-06-08 18:35:52 +0530

    replace view flipper with view pager

--- dvu/src/main/java/github/com/vikramezhil/dvu/views/flipper/DvuFlipperView.kt
+++ dvu/src/main/java/github/com/vikramezhil/dvu/views/flipper/DvuFlipperView.kt
@@ -1,101 +1,54 @@
 package github.com.vikramezhil.dvu.views.flipper
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.view.MotionEvent
-import android.view.animation.AnimationUtils
-import android.widget.ViewFlipper
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.AttrRes
-import github.com.vikramezhil.dvu.R
+import androidx.core.view.get
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 
 /**
  * Droid View Utils - Flipper View
  * @author vikramezhil
  */
 
-class DvuFlipperView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, @AttrRes defStyleAttr: Int = 0): ViewFlipper(context)  {
-
-    private val properties: DvuFvProps = object: DvuFvProps() {
-        override var initialX: Float = 0.0f
-
-        override var infiniteSwipe: Boolean = false
-            set(value) {
-                field = value
-                invalidate()
-            }
-    }
+class DvuFlipperView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, @AttrRes defStyleAttr: Int = 0): ViewPager(context) {
 
     private var listener: OnDvuFvListener? = null
 
     init {
-        init(context, attrs)
-    }
+        // Listening for page changes
+        addOnPageChangeListener(object: OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
 
-    /**
-     * Initializes the view attributes
-     * @param context Context The view context
-     * @param attrs AttributeSet The view attributes
-     */
-    private fun init(context: Context, attrs: AttributeSet?) {
-        if (attrs == null) return
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
 
-        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.DvuFlipperView, 0, 0)
-
-        try {
-            properties.infiniteSwipe = typedArray.getBoolean(R.styleable.DvuFlipperView_dvuFvInfiniteSwipe, properties.infiniteSwipe)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            typedArray.recycle()
-        }
-    }
-
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        super.dispatchTouchEvent(ev)
-        return true
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        event?.let {
-            when(it.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    properties.initialX = it.x
-                }
-                MotionEvent.ACTION_UP -> {
-                    val finalX = it.x
-                    if (properties.initialX > finalX) {
-                        if ((displayedChild == (childCount - 1)) && !properties.infiniteSwipe) {
-                            // Reached end
-                            return false
-                        }
-
-                        inAnimation = AnimationUtils.loadAnimation(context, R.anim.left_in)
-                        outAnimation = AnimationUtils.loadAnimation(context, R.anim.left_out)
-                        showNext()
-                    } else {
-                        if (displayedChild == 0 && !properties.infiniteSwipe) {
-                            // Reached start
-                            return false
-                        }
-
-                        inAnimation = AnimationUtils.loadAnimation(context, R.anim.right_in)
-                        outAnimation = AnimationUtils.loadAnimation(context, R.anim.right_out)
-                        showPrevious()
-                    }
-
-                    // Sending an update back on the current visible view
-                    listener?.onCurrentViewVisible(currentView, displayedChild)
-                } else -> {
-                    return false
-                }
+            override fun onPageSelected(position: Int) {
+                listener?.onPageSelected(position)
             }
-        } ?: run {
-            return false
-        }
+        })
+    }
 
-        return true
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        val childrenCount = childCount
+        offscreenPageLimit = (childrenCount - 1)
+        adapter = object: PagerAdapter() {
+            override fun instantiateItem(container: ViewGroup, position: Int): Any {
+                return container[position]
+            }
+
+            override fun isViewFromObject(view: View, obj: Any): Boolean {
+                return view == obj
+            }
+
+            override fun getCount(): Int {
+                return childrenCount
+            }
+        }
     }
 
     /**
--- dvu/src/main/java/github/com/vikramezhil/dvu/views/flipper/OnDvuFvListener.kt
+++ dvu/src/main/java/github/com/vikramezhil/dvu/views/flipper/OnDvuFvListener.kt
@@ -1,12 +1,9 @@
 package github.com.vikramezhil.dvu.views.flipper
 
-import android.view.View
-
 interface OnDvuFvListener {
     /**
-     * Sends the current view visible data
-     * @param view View The current visible view
+     * Sends an update when a page is selected
      * @param position Int The current visible view position
      */
-    fun onCurrentViewVisible(view: View, position: Int)
+    fun onPageSelected(position: Int)
 }
